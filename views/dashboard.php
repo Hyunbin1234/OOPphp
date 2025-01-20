7ea2e3d615a3b1b8d5255e895d12f4717bdc5de7
Hyunbin1234 <maayhelda@gmail.com>
2025-01-20 14:18:24 +0700

    second commit

--- views/dashboard.php
+++ views/dashboard.php
@@ -21,9 +21,31 @@ include 'partials/header.php';
                 class="d-inline-block">
             Nk Film Animasion Indonesia™
         </a>
-        <form method="POST">
+
+        <!-- <form method="POST">
             <button type="submit" name="logout" class="btn btn-danger">logout</button>
-        </form>
+        </form> -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  logout
+</button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">you sure you want Logout?!!</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">logout</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
 </nav>
 
